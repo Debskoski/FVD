44dd650447fa02826fc91ba365f77c70fe25d5d1
Debskoski <deboraprophitius@gmail.com>
2024-09-12 11:40:44 +0200

    jaaa
    alles aangepast

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,8 @@
-// JavaScript Document
-console.log("hi");
+// Selecteer de hamburgerknop en de navigatie
+const hamburger = document.querySelector('nav button');
+const nav = document.querySelector('nav');
+
+// Voeg een event listener toe aan de hamburgerknop
+hamburger.addEventListener('click', function() {
+    nav.classList.toggle('active'); // Toggle de 'active' klasse
+});
